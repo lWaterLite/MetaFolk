37fa8a88535b811dd5cbd2b9e6e51c411d32eee6
Kang SHiqi <3509148389@qq.com>
2023-01-11 09:37:48 +0800

    Repaired the getWhole part Enlarged the range of indexes of MusicList

--- PythonSpiderForQQMusic/getWhole.py
+++ PythonSpiderForQQMusic/getWhole.py
@@ -1,20 +1,23 @@
-from Single import getSingle
-from Single import Song
+
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 
 
 def getWhole(Id) -> list:
-    URL = 'https://y.qq.com/n/ryqq/search?w=' + Id + '&t=album&remoteplace=txt.yqq.center'
+    URL = 'https://y.qq.com/n/ryqq/search?w='+Id+'&t=playlist&remoteplace=txt.yqq.playlist'
     MusicList = webdriver.Edge()
     MusicList.get(url=URL)
     input('Please enter an notification to indicate the success of login')
     ListOfList = []
-    ListRange = ['1', '2', '3']
+    ListRange = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     for index in ListRange:
-        xPath = '/html/body/div/div/div[3]/div/div/div[2]/ul[2]/li[' + index + ']/h4/span/a'
-        ListURL = MusicList.find_element(by=By.XPATH, value=xPath).get_attribute('href')
-        ListOfList.append(ListURL)
+        try:
+            xPath = '/html/body/div/div/div[3]/div/div/div[2]/ul[2]/li[' + index + ']/h4/span/a'
+            ListURL = MusicList.find_element(by=By.XPATH, value=xPath).get_attribute('href')
+            ListOfList.append(ListURL)
+        except NoSuchElementException:
+            pass
     MusicList.close()
     return ListOfList
