8b5d6b0483f20c200d3f4584f9507dbd1f8bc312
Kang SHiqi <3509148389@qq.com>
2023-01-12 16:03:22 +0800

    Standardization

--- PythonSpiderForQQMusicUsingSelenium/main.py
+++ PythonSpiderForQQMusicUsingSelenium/main.py
@@ -1,9 +1,10 @@
 from getWhole import getWhole
 from MusicList import getMusicList
 
-Id = input("enter the keyword for you spider request")
-ListOfList = getWhole(Id)
-for i in range(1, 4):
-    getMusicList(ListOfList[i])
+if __name__ == '__main__':
+    Id = input("enter the keyword for you spider request")
+    ListOfList = getWhole(Id)
+    for i in range(1, 4):
+        getMusicList(ListOfList[i])
 
 
