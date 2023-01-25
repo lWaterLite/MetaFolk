af32f6abc3abf4649f1e5f95dbe18b34d8e9506c
Kangshiqi0108 <3509148389@qq.com>
2023-01-25 13:03:11 +0800

    adding database store function complete the password in line 29 first

--- PythonSpiderForQQMusicUsingSelenium/main.py
+++ PythonSpiderForQQMusicUsingSelenium/main.py
@@ -3,29 +3,61 @@ import time
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+import pymysql
 
 MusicList = []
 check = dict()
 
 
 class Song:
-    name = ''
+    Name = ''
     Singer = ''
     lyrics = ''
     MediaUrl = ''
     SongMId = ''
 
     def show(self):
-        print("name : " + self.name)
+        print("name : " + self.Name)
         print("Singer : " + self.Singer)
-        print("Lyrics : " + self.Lyrics)
+        print("Lyrics : " + self.lyrics)
         print("mediaUrl:" + self.MediaUrl)
 
 
 def store():
     with open('songList.json', 'w', encoding='utf-8') as fp:
         for song in MusicList:
-            json.dump(obj=song.__dict__, fp=fp, ensure_ascii=False)
+            db = pymysql.connect(host='localhost', user='root', password='', database='metafolk')
+            cursor = db.cursor()
+            insert_Song = "insert into song(song_name) values('{song_name}');".format(song_name=song.songName)
+
+            cursor.execute(insert_Song)
+
+            get_song_id = "select song_id from song order by song_id desc limit 1;"
+
+            cursor.execute(get_song_id)
+
+            insert_item_song = "insert into item_song(song_ref_id, item_singer) values({song_ref_id}, " \
+                               "'{item_singer}');".format(
+                song_ref_id=cursor.fetchone()[0], item_singer=song.artists)
+
+            cursor.execute(insert_item_song)
+
+            get_item_id = "select item_id from item_song order by item_id desc limit 1;"
+
+            cursor.execute(get_item_id)
+
+            insert_item_lyrics = "insert into item_lyrics(item_ref_id, lyrics) values({item_ref_id}, " \
+                                 "'{lyrics}');".format(
+                item_ref_id=cursor.fetchone()[0], lyrics=song.lyric)
+
+            cursor.execute(insert_item_lyrics)
+
+        db.commit()
+
+        cursor.close()
+
+        db.close()
+        json.dump(obj=song.__dict__, fp=fp, ensure_ascii=False)
 
 
 def getSingle(Id) -> Song:
@@ -34,25 +66,25 @@ def getSingle(Id) -> Song:
         Page = webdriver.Edge()
         Page.get(url=URL)
         Single = Song()
-        Page.implicitly_wait(5)
+        time.sleep(3)
         a = Page.find_element(
             By.XPATH, '/html/body/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/a')
         Page.execute_script("arguments[0].click();", a)
-        time.sleep(1)
-        Single.name = Page.find_element(
+        Page.implicitly_wait(5)
+        Single.Name = Page.find_element(
             by=By.XPATH, value='/html/body/div/div/div[2]/div[1]/div/div[1]/h1').text
         Single.Singer = Page.find_element(
             by=By.XPATH, value='/html/body/div/div/div[2]/div[1]/div/div[2]/a').text
         Lyrics = Page.find_element(
             by=By.XPATH, value='/html/body/div/div/div[2]/div[2]/div[1]/div[1]/div[2]/div')
-        Single.Lyrics = Lyrics.text
+        Single.lyrics = Lyrics.text
         Page.close()
         index = URL.index('songDetail/')
-        Id = ''
-        for i in range(index + 11, len(URL)):
-            Id = Id + URL[i]
-        Single.SongMId = Id
-        MobileUrl = 'https://i.y.qq.com/v8/playsong.html?songmid={}'.format(Id)
+        ID = ''
+        for index in range(index + 11, len(URL)):
+            ID = ID + URL[index]
+        Single.SongMId = ID
+        MobileUrl = 'https://i.y.qq.com/v8/playsong.html?songmid={}'.format(ID)
         options = webdriver.EdgeOptions()
         mobileEmulation = {"deviceName": "iPhone 6"}
         options.add_experimental_option("mobileEmulation", mobileEmulation)
@@ -61,58 +93,58 @@ def getSingle(Id) -> Song:
         Mobile.implicitly_wait(5)
         MediaUrl = Mobile.find_element(by=By.XPATH, value='/html/body/audio')
         Single.MediaUrl = MediaUrl.get_attribute('src')
-        if check.get(Single.name + Single.Singer, False):
+        if check.get(Single.Name + Single.Singer, False):
             pass
         else:
-            check[Single.name + Single.Singer] = True
+            check[Single.Name + Single.Singer] = True
             MusicList.append(Single)
             Mobile.close()
             Single.show()
         return Single
     except NoSuchElementException:
         Error = Song()
-        Error.name = 'Error'
+        Error.Name = 'Error'
         Error.Singer = 'Error'
-        Error.Lyrics = 'Error'
+        Error.lyrics = 'Error'
         Error.SongMId = 'Error'
         Error.MediaUrl = 'Error'
         return Error
 
 
-def getMusicList(Id) -> list:
-    URL = Id
-    MusicList = webdriver.Edge()
-    MusicList.get(url=URL)
+def getMusicList(id) -> list:
+    URL = id
+    MusicListPage = webdriver.Edge()
+    MusicListPage.get(url=URL)
     time.sleep(5)
     SingleList = []
     ListRange = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     try:
         for index in ListRange:
             xPath = '/html/body/div/div/div[2]/div[2]/div[1]/div[1]/ul[2]/li[' + index + ']/div/div[2]/span/a'
-            SingleURL = MusicList.find_element(by=By.XPATH, value=xPath).get_attribute('href')
+            SingleURL = MusicListPage.find_element(by=By.XPATH, value=xPath).get_attribute('href')
             Single = getSingle(SingleURL)
             SingleList.append(Single)
     except NoSuchElementException:
         pass
-    MusicList.close()
+    MusicListPage.close()
     return SingleList
 
 
 def getWhole(Id) -> list:
     URL = 'https://y.qq.com/n/ryqq/search?w=' + Id + '&t=playlist&remoteplace=txt.yqq.playlist'
-    MusicList = webdriver.Edge()
-    MusicList.get(url=URL)
+    MusicListPage = webdriver.Edge()
+    MusicListPage.get(url=URL)
     input('Please enter an notification to indicate the success of login')
     ListOfList = []
     ListRange = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     for index in ListRange:
         try:
             xPath = '/html/body/div/div/div[3]/div/div/div[2]/ul[2]/li[' + index + ']/h4/span/a'
-            ListURL = MusicList.find_element(by=By.XPATH, value=xPath).get_attribute('href')
+            ListURL = MusicListPage.find_element(by=By.XPATH, value=xPath).get_attribute('href')
             ListOfList.append(ListURL)
         except NoSuchElementException:
             pass
-    MusicList.close()
+    MusicListPage.close()
     return ListOfList
 
 
